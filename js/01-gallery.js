7d70ab47f539210dab723d931d21be3a0dcb9505
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-12-09 15:56:10 +0200

    hw- 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.addEventListener('click', onClickImg);
+onCreatGallery();
+function onCreatGallery() {
+  const galleryItemsEl = galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      (acc += `
+    <div class="gallery__item">
+     <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+  </div>`),
+    ''
+  );
+
+  galleryEl.insertAdjacentHTML('beforeend', galleryItemsEl);
+}
+
+function onClickImg(evt) {
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  const img = evt.target;
+  onModalSource(img.dataset.source, img.alt);
+}
+let modalSource;
+function onModalSource(source, alt) {
+  modalSource = basicLightbox.create(
+    `
+    <img src="${source}"alt='${alt}'>
+`,
+    {
+      onShow: () => document.addEventListener('keydown', onClose),
+    },
+    {
+      onClose: () => document.removeEventListener('keydown', onClose),
+    }
+  );
+  modalSource.show();
+}
+
+function onClose(evt) {
+  if (evt.code !== 'Escape') {
+    return;
+  }
+  modalSource.close();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEL = document.querySelector('.gallery');
+
+onCreatGallery();
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  close: true,
+});
+
+function onCreatGallery() {
+  const galleryItemsEl = galleryItems.reduce(
+    (acc, { preview, original, description }) =>
+      (acc += `
+ <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+   `),
+    ''
+  );
+  galleryEL.insertAdjacentHTML('beforeend', galleryItemsEl);
+  // console.log(galleryItemsEl);
+}
